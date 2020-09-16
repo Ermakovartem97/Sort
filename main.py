f06d7ac3acc71e8c8813b047633a9cf6df472cf0
Ermakov Artem <ermakovartem97@mail.ru>
2020-09-16 17:58:45 +0300

    Обновление v0.5
    Доделал сортировку пузырьком, начал делать шейкерную сортировку.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from stupid import sort_fun
+from stupid import sort_stupid
+from bubble import sort_bubble
+from cocktail import sort_cocktaill
 
 from testSort import test_sort
 
-print("Глупая сортировка: ", test_sort(sort_fun), " сложность O(n3) = ", 400**3)
+print("Параметры сортировки: 100 списков по 400 чисел от -100 до 100")
+print("Глупая сортировка: ", test_sort(sort_stupid), " сложность O(n3)")
+print("Cортировка пузырьком: ", test_sort(sort_bubble), " сложность O(n3)")
+print("Шейкерная сортировка: ", test_sort(sort_cocktaill), " сложность O(n3)")
+
